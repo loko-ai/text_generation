d643753706c0c9095856b41aa21d40aff7b855cd
robertaparisi <roberta.parisi2@gmail.com>
2022-11-25 18:33:31 +0100

    added parameters

---
+++ extensions/components.py
@@ -0,0 +1,20 @@
+from loko_extensions.model.components import Component, save_extensions, Arg, Dynamic
+
+max_length = Arg(name="max_length", label="Sentence Max Length", type="number", helper="Integer number")
+
+no_rep_ngrams_size = Arg(name="ngram_size", label="No Repeat N-Gram size", type="number", value=2,
+                         helper="Integer number")  # , description="Size of t")
+
+do_sample = Arg(name="do_sample", label="Sampling", type="boolean", value=True,
+                description="If false, the text will be generated in a deterministic way. Otherwise, the words will be picked according to their conditional probability.")
+
+temperature = Dynamic(name="temperature", label="Temperature", dynamicType="number", value=0.3, parent="do_sample",
+        condition="{parent}===true", helper="Float positive number. Max value 1.0",
+        description='With temperature=1, all the words will be picked only considering their probability. Lower "Temperature" values will make the distribution less random. The sentence will probabily be more logic, but with long text you can encounter more repetition. Use your perfect trade-off value.')
+
+args = [max_length, no_rep_ngrams_size, do_sample, temperature]
+
+text_gen_component = Component(name="TextGenerator", args=args)
+
+if __name__ == '__main__':
+    save_extensions([text_gen_component])
--- services/services.py
+++ services/services.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, jsonify
 from loko_extensions.business.decorators import extract_value_args
-from loko_extensions.model.components import Component, save_extensions
 from transformers import pipeline
+from utils.logger_utils import logger
 
 app = Flask("")
-a = Component(name="hf")
 
 generator = pipeline(task="text-generation")
-
-save_extensions([a])
 
 
 @app.route("/", methods=["POST"])
 @extract_value_args(_request=request)
 def test(value, args):
-    return jsonify(generator(value))
+    logger.debug(f"args::: {args}")
+    max_length = int(args.get("max_length")) if args.get("max_length") else None
+    ngram_size = int(args.get("ngram_size")) if args.get("ngram_size", None) else None
+    do_sample = args.get("do_sample", False)
+    temperature = float(args.get("temperature", 0.7)) if args.get("temperature", 0.7) else None
+    return jsonify(generator(value, max_length=max_length, no_repeat_ngram_size=ngram_size, do_sample=do_sample, temperature=temperature))
 
 
 if __name__ == "__main__":
